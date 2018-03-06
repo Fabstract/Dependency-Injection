3c7725edc16bf4415937614467477e84fb0a4f27
Ahmet Türk <ahmetturk93@gmail.com>
2018-03-06 11:36:02 +0300

    ADD Constraint

--- src/Container.php
+++ src/Container.php
@@ -10,6 +10,8 @@ class Container implements ContainerInterface
      * @var ServiceDefinition[]
      */
     protected $service_lookup = [];
+    /** @var string[] */
+    protected $constraint_lookup = [];
 
     /**
      * @param string $name
@@ -38,7 +40,13 @@ class Container implements ContainerInterface
     {
         Assert::isDefinition($definition);
 
-        $this->service_lookup[$definition->getName()] = $definition;
+        $service_name = $definition->getName();
+        if (array_key_exists($service_name, $this->constraint_lookup) === true) {
+            $type = $this->constraint_lookup[$service_name];
+            Assert::isType($definition, $type, 'definition');
+        }
+
+        $this->service_lookup[$service_name] = $definition;
         $definition->setContainer($this);
         return $this;
     }
@@ -55,5 +63,19 @@ class Container implements ContainerInterface
         }
 
         return $this->service_lookup[$name];
+    }
+
+    /**
+     * @param string $service_name
+     * @param string $constraint_class
+     * @return $this
+     */
+    public function addServiceDefinitionConstraint($service_name, $constraint_class)
+    {
+        Assert::isNotNullOrWhiteSpace($service_name, 'service_name');
+        Assert::isClassExists($constraint_class, 'constraint_class');
+
+        $this->constraint_lookup[$service_name] = $constraint_class;
+        return $this;
     }
 }
